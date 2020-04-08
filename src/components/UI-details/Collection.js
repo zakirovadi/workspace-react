755e46be1fdab0c8516441f664f4450540a61df1
Diana Zakirova <zakirovadiana.mk@gmail.com>
2020-04-08 16:03:04 +0300

    create collections

--- src/components/UI-details/Collection.js
+++ src/components/UI-details/Collection.js
@@ -2,8 +2,40 @@ import React from 'react';
 import './Collection.scss';
 
 function Collection(props) {
+
+    const products = props.collection.map((product, index) => {
+        if(index < 3){
+            return (
+                <div className="product" key={index}>
+                    <div className="img">
+                        <img src={product.image} alt={product.name} />
+
+                        {index < 2
+                            ? <div className="line"></div>
+                            :null
+                        }
+                    </div>
+    
+                    <div className="title">
+                        <h3>{product.name}</h3>
+                        <span>${product.price}</span>
+                    </div>
+                </div>
+            );
+        }else{return null}
+    });
+
+
     return(
-        <div>All Collections</div>
+        <div className="collection">
+            <div className="name">
+                <h2>{props.name}</h2>
+                <span className="text-underline">See more</span>
+            </div>
+            <div className="products">
+                {products}
+            </div>
+        </div>
     );
 }
 
--- src/components/pages/Collections.js
+++ src/components/pages/Collections.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import './Collections.scss'
+import Collection from '../UI-details/Collection';
+
+import NEWARRIVALS from '../../shared/collections/newarrivals';
+import DESKSANDTABLES from '../../shared/collections/desksandtables';
+import CHAIRS from '../../shared/collections/chairs';
+import ORGANIZERS from '../../shared/collections/organizers';
+import WALLSHELVES from '../../shared/collections/wallshelves';
 
 function Collections(props) {
     return(
-        <div>All Collections</div>
+        <div className="container collections">
+            <Collection name="New Arrivals" collection = {NEWARRIVALS}/>
+            <div className="hr-line"></div>
+            <Collection name="Desk & Tables" collection = {DESKSANDTABLES}/>
+            <div className="hr-line"></div>
+            <Collection name="Chairs" collection = {CHAIRS}/>
+            <div className="hr-line"></div>
+            <Collection name="Organizers" collection = {ORGANIZERS}/>
+            <div className="hr-line"></div>
+            <Collection name="Wall Shelves" collection = {WALLSHELVES}/>            
+        </div>
     );
 }
 
